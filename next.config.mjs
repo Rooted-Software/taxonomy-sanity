e124fb7b8af060d06e22a1ddae160b659a63911f
Marshal Morse <marshalmorse@gmail.com>
2023-03-07 09:31:55 -0600

    wip-pre-merge

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,22 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      { hostname: 'cdn.sanity.io' },
+      { hostname: 'source.unsplash.com' },
+    ],
   },
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
 }
 
