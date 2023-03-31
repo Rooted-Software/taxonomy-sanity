9bde260088a1927af1c6e0f321839ff75999b135
Marshal Morse <marshalmorse@gmail.com>
2023-03-30 21:33:32 -0500

    camera-test

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,5 +1,23 @@
 import Head from "next/head";
 
+import { StreamButton } from "@/components/webcam/stream-button";
+
+async function getAccess() { 
+
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true 
+    });
+  
+      const videoTracks = stream.getVideoTracks()
+      const track = videoTracks[0]
+      alert(`Getting video from: ${track.label}`)
+      document.querySelector('video').srcObject = stream
+
+  //The video stream is stopped by track.stop() after 3 second of playback.
+     
+}
+
 export default function Offline() {
   return (
     <>
@@ -8,6 +26,12 @@ export default function Offline() {
       </Head>
       <h1>This is offline fallback page</h1>
       <h2>When offline, any page route will fallback to this page</h2>
+      <div><input type="file" accept="image/x-png,image/jpeg,image/gif"/></div>
+      <div><input type="text" id="name" /></div>
+      <input type="submit"></input>
+      <StreamButton />
+<video></video>
+<script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
     </>
   );
 }
---
+++ components/webcam/stream-button.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import * as React from 'react'
+
+import { Icons } from '@/components/icons'
+import { cn } from '@/lib/utils'
+import { toast } from '@/ui/toast'
+
+interface StreamButtonProps
+  extends React.HTMLAttributes<HTMLButtonElement> {}
+
+export function StreamButton({
+  className,
+  ...props
+}: StreamButtonProps) {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const [vidStream, setVidStream] = React.useState<any>('')
+  
+  async function onClick() {
+    const width = '320';
+    setIsLoading(true)
+    setIsLoading(false)
+    let constraints = {
+        audio: false,
+        video: {
+          facingMode: 'environment'
+        }
+      };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  
+        const devices = await navigator.mediaDevices.enumerateDevices(); 
+        console.log(devices); 
+      const videoTracks = stream.getVideoTracks()
+      console.log(videoTracks); 
+      const track = videoTracks[0]
+
+      alert(`Getting video from: ${track.label}`)
+      setVidStream(stream)
+     const video =  document.querySelector('video'); 
+      document.querySelector('video').srcObject = stream
+      document.querySelector('video').play()
+      const height = ((video.videoHeight / video.videoWidth) * parseInt(width)).toString();
+
+      const canvas =  document.querySelector('canvas'); 
+      video.setAttribute("width", width);
+      video.setAttribute("height", height);
+      canvas.setAttribute("width", width);
+      canvas.setAttribute("height", height);
+    }
+
+ async function getFrame() {
+    const width = '320';
+    const canvas =  document.querySelector('canvas'); 
+    const context = canvas.getContext("2d");
+    const video =  document.querySelector('video'); 
+    const height = ((video.videoHeight / video.videoWidth) * parseInt(width)).toString();
+
+   
+  if (width && height) {
+    canvas.width = parseInt(width);
+    canvas.height = parseInt(height);
+    context.drawImage(video, 0, 0, parseInt(width), parseInt(height));
+
+    const data = canvas.toDataURL("image/png");
+    const photo =  document.querySelector('photo'); 
+    photo.setAttribute("src", data);
+    }
+ }
+  return (
+    <div>
+    <button
+      onClick={onClick}
+      className={cn(
+        'relative inline-flex h-9 items-center rounded-md border border-transparent bg-brand-500 px-4 py-2 text-sm font-medium text-white hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2',
+        {
+          'cursor-not-allowed opacity-60': isLoading,
+        },
+        className
+      )}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <Icons.add className="mr-2 h-4 w-4" />
+      )}
+      Stream
+    </button>
+    <button
+      onClick={getFrame}
+      className={cn(
+        'relative inline-flex h-9 items-center rounded-md border border-transparent bg-brand-500 px-4 py-2 text-sm font-medium text-white hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2',
+        {
+          'cursor-not-allowed opacity-60': isLoading,
+        },
+        className
+      )}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <Icons.add className="mr-2 h-4 w-4" />
+      )}
+      Get Frame
+    </button>
+    <canvas id="canvas"> </canvas>
+    <video id="video"> </video>
+<script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+<div className="output">
+  <img id="photo" alt="The screen capture will appear in this box." src=''/>
+</div>
+    </div>
+  )
+}
