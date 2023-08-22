9afa8347b0c72bc4e06f0fbcca521dcbfd213b05
Marshal Morse <marshalmorse@gmail.com>
2023-08-22 10:59:47 -0500

    fix-layout

--- app/(private)/projectMapping/layout.tsx
+++ app/(private)/projectMapping/layout.tsx
@@ -20,7 +20,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="mx-auto flex flex-col space-y-6">
-      <header className="container sticky top-0 z-40 bg-white">
+      <header className="container sticky top-0 z-40 bg-background">
         <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
           <MainNav items={dashboardConfig.mainNav} />
           <UserAccountNav
@@ -32,14 +32,11 @@ export default async function DashboardLayout({
           />
         </div>
       </header>
-      <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav items={dashboardConfig.sidebarNav} />
-        </aside>
+ 
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           {children}
         </main>
-      </div>
+
     </div>
   )
 }
