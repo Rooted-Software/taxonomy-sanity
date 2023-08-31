e9a5760b80f5179a038b2a0476d93ab7b24c9351
Peter Steffey <peter@steffey.dev>
2023-08-31 18:53:44 -0400

    Combine reJournalEntryBatches & feGiftBatches syncing logic

--- lib/virGifts.ts
+++ lib/virGifts.ts
@@ -37,7 +37,7 @@ export const dateFilterOptions = Object.keys(virDateOptions).map((key) =>
   parseInt(key)
 )
 
-// attempt to get batches, but if there are none, request them from Virtuous
+// Get batches from local DB and check Virtuous for any new ones
 export const getVirtuousBatches = async (teamId, dateFilter = 30) => {
   const filterDate = new Date()
   filterDate.setDate(filterDate.getDate() - dateFilter - 1)
@@ -73,7 +73,7 @@ export const getVirtuousBatches = async (teamId, dateFilter = 30) => {
       sortBy: 'Last Modified Date',
       descending: 'true',
     }
-    
+
     const res = await virApiFetch(
       'https://api.virtuoussoftware.com/api/Gift/Query?skip=0&take=1000',
       'POST',
@@ -170,12 +170,14 @@ export const getVirtuousBatch = async (teamId, batchId: string) => {
     // TODO: if already synced, run diff and return diff report if anything has changed
     const giftsFromVirtuous = (await res.json()).list
     const giftsLocal = batch.synced
-      ? (await db.gift.findMany({
-          where: {
-            teamId: teamId,
-            batch_name: batch.batch_name,
-          },
-        })).map(gift => ({ ...gift, amount: gift.amount?.toNumber() }))
+      ? (
+          await db.gift.findMany({
+            where: {
+              teamId: teamId,
+              batch_name: batch.batch_name,
+            },
+          })
+        ).map((gift) => ({ ...gift, amount: gift.amount?.toNumber() }))
       : undefined
 
     return {
@@ -184,7 +186,7 @@ export const getVirtuousBatch = async (teamId, batchId: string) => {
     }
   }
 
-  throw new Error("Batch not found");
+  throw new Error('Batch not found')
 }
 
 export async function updateGiftBatch(batchName, reBatchNo, teamId) {
@@ -207,60 +209,63 @@ export async function updateGiftBatch(batchName, reBatchNo, teamId) {
 }
 
 export async function insertGifts(gifts, teamId, batch_name) {
-  return await db.gift.createMany({
-    data: gifts.map((gift) => ({
-      id: gift.id,
-      transactionSource: gift.transactionSource,
-      transactionId: gift.transactionId,
-      contactId: gift.contactId,
-      contactName: gift.contactName,
-      contactUrl: gift.contactUrl,
-      giftType: gift.giftType,
-      giftTypeFormatted: gift.giftTypeFormatted,
-      giftDate: new Date(gift.giftDate),
-      giftDateFormatted: gift.giftDateFormatted,
-      amount: gift.amount,
-      amountFormatted: gift.amountFormatted,
-      currencyCode: gift.currencyCode,
-      exchangeRate: gift.exchangeRate,
-      baseCurrencyCode: gift.baseCurrencyCode,
-      batch: gift.batch,
-      createDateTimeUtc: new Date(gift.createDateTimeUtc),
-      createdByUser: gift.createdByUser,
-      modifiedDateTimeUtc: new Date(gift.modifiedDateTimeUtc),
-      modifiedByUser: gift.modifiedByUser,
-      segmentId: gift.segmentId,
-      segment: gift.segment,
-      segmentCode: gift.segmentCode,
-      segmentUrl: gift.segmentUrl,
-      mediaOutletId: gift.mediaOutletId,
-      mediaOutlet: gift.mediaOutlet,
-      grantId: gift.grantId,
-      grant: gift.grant,
-      grantUrl: gift.grantUrl,
-      notes: gift.notes,
-      tribute: gift.tribute,
-      tributeId: gift.tributeId,
-      tributeType: gift.tributeType,
-      acknowledgeIndividualId: gift.acknowledgeIndividualId,
-      receiptDate: new Date(gift.receiptDate),
-      receiptDateFormatted: gift.receiptDateFormatted,
-      contactPassthroughId: gift.contactPassthroughId,
-      contactPassthroughUrl: gift.contactPassthroughUrl,
-      contactIndividualId: gift.contactIndividual,
-      cashAccountingCode: gift.cashAccountingCode,
-      giftAskId: gift.giftAskId,
-      contactMembershipId: gift.contactMembershipId,
-      giftUrl: gift.giftUrl,
-      isTaxDeductible: gift.isTaxDeductible,
-      giftDesignations: gift.giftDesignations,
-      giftPremiums: gift.giftPremiums,
-      recurringGiftPayments: gift.recurringGiftPayments,
-      pledgePayments: gift.pledgePayments,
-      customFields: gift.customFields,
-      batch_name: gift.batch || 'none',
-      synced: true,
-      teamId,
-    })),
-  })
+  return await db.gift.createMany(
+    {
+      data: gifts.map((gift) => ({
+        id: gift.id,
+        transactionSource: gift.transactionSource,
+        transactionId: gift.transactionId,
+        contactId: gift.contactId,
+        contactName: gift.contactName,
+        contactUrl: gift.contactUrl,
+        giftType: gift.giftType,
+        giftTypeFormatted: gift.giftTypeFormatted,
+        giftDate: new Date(gift.giftDate),
+        giftDateFormatted: gift.giftDateFormatted,
+        amount: gift.amount,
+        amountFormatted: gift.amountFormatted,
+        currencyCode: gift.currencyCode,
+        exchangeRate: gift.exchangeRate,
+        baseCurrencyCode: gift.baseCurrencyCode,
+        batch: gift.batch,
+        createDateTimeUtc: new Date(gift.createDateTimeUtc),
+        createdByUser: gift.createdByUser,
+        modifiedDateTimeUtc: new Date(gift.modifiedDateTimeUtc),
+        modifiedByUser: gift.modifiedByUser,
+        segmentId: gift.segmentId,
+        segment: gift.segment,
+        segmentCode: gift.segmentCode,
+        segmentUrl: gift.segmentUrl,
+        mediaOutletId: gift.mediaOutletId,
+        mediaOutlet: gift.mediaOutlet,
+        grantId: gift.grantId,
+        grant: gift.grant,
+        grantUrl: gift.grantUrl,
+        notes: gift.notes,
+        tribute: gift.tribute,
+        tributeId: gift.tributeId,
+        tributeType: gift.tributeType,
+        acknowledgeIndividualId: gift.acknowledgeIndividualId,
+        receiptDate: new Date(gift.receiptDate),
+        receiptDateFormatted: gift.receiptDateFormatted,
+        contactPassthroughId: gift.contactPassthroughId,
+        contactPassthroughUrl: gift.contactPassthroughUrl,
+        contactIndividualId: gift.contactIndividual,
+        cashAccountingCode: gift.cashAccountingCode,
+        giftAskId: gift.giftAskId,
+        contactMembershipId: gift.contactMembershipId,
+        giftUrl: gift.giftUrl,
+        isTaxDeductible: gift.isTaxDeductible,
+        giftDesignations: gift.giftDesignations,
+        giftPremiums: gift.giftPremiums,
+        recurringGiftPayments: gift.recurringGiftPayments,
+        pledgePayments: gift.pledgePayments,
+        customFields: gift.customFields,
+        batch_name: gift.batch || 'none',
+        synced: true,
+        teamId,
+      })),
+      skipDuplicates: true
+    }
+  )
 }
