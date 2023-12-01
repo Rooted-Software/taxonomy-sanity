25cd4fed55e96983ec7c98583df8e53697c7d390
Peter Steffey <peter@steffey.dev>
2023-12-01 11:27:21 -0500

    Simpler login / signup form

--- components/user-virtuous-auth-form.tsx
+++ components/user-virtuous-auth-form.tsx
@@ -82,87 +82,84 @@ export function UserVirtuousAuthForm({
   return (
     <div className={cn('grid gap-6', className)} {...props}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="center-items xs:grid-cols-2  grid w-full gap-5 md:grid-cols-4">
-          <div className="col-span-2 md:col-span-1"> &nbsp;</div>
-          <div className="col-span-2 grid md:col-span-1 ">
-            <label className="my-2 text-xs" htmlFor="email">
-              Email
-            </label>
-            <input
-              id="email"
-              placeholder="name@example.com"
-              className="my-0 mb-2 block h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-black placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-              {...register('email')}
-            />
-            {errors?.email && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.email.message}
-              </p>
-            )}
+        {twoFactorForm ? (
+          <>
+            <div className="w-100 grid md:mt-8 md:pt-4"></div>
+            <div></div>
+            <div className="col-span-2 grid">
+              {' '}
+              <label className="my-2 text-xs" htmlFor="twoFactor">
+                Two Factor
+              </label>
+              <input
+                id="twoFactor"
+                placeholder="123456"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                type="text"
+                autoCapitalize="none"
+                autoCorrect="off"
+                disabled={isLoading}
+                {...register('twoFactor')}
+              />
+            </div>
+          </>
+        ) : (
+          <div className="mb-3 space-y-1">
+            <div>
+              <label className="text-xs" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                id="email"
+                placeholder="name@example.com"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-black placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+                {...register('email')}
+              />
+              {errors?.email && (
+                <p className="px-1 text-xs text-red-600">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="my-2 text-xs" htmlFor="password">
+                Password
+              </label>
+              <input
+                id="password"
+                placeholder="password"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                type="password"
+                autoCapitalize="none"
+                autoCorrect="off"
+                disabled={isLoading}
+                {...register('password')}
+              />
+              {errors?.password && (
+                <p className="px-1 text-xs text-red-600">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
           </div>
-          <div className="col-span-2 grid gap-1 md:col-span-1 ">
-            <label className="my-2 text-xs" htmlFor="password">
-              Password
-            </label>
-            <input
-              id="password"
-              placeholder="password"
-              className="my-0 mb-2 block h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
-              type="password"
-              autoCapitalize="none"
-              autoCorrect="off"
-              disabled={isLoading}
-              {...register('password')}
-            />
-            {errors?.password && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-          {twoFactorForm && (
-            <>
-              <div className="w-100 grid md:mt-8 md:pt-4"></div>
-              <div></div>
-              <div className="col-span-2 grid">
-                {' '}
-                <label className="my-2 text-xs" htmlFor="twoFactor">
-                  Two Factor
-                </label>
-                <input
-                  id="twoFactor"
-                  placeholder="123456"
-                  className="my-0 mb-2 block h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
-                  type="text"
-                  autoCapitalize="none"
-                  autoCorrect="off"
-                  disabled={isLoading}
-                  {...register('twoFactor')}
-                />
-              </div>
-            </>
+        )}
+
+        <button
+          className="col-span-2 mx-auto inline-flex w-full items-center justify-center justify-self-center rounded-full bg-accent-1 px-5 py-2.5 text-center text-sm font-medium text-dark hover:bg-cyan focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 disabled:opacity-50 dark:hover:bg-[#050708]/30 dark:focus:ring-slate-500 md:mt-1 "
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Icons.key className="mr-2 h-4 w-4 " />
           )}
-
-          <div className="w-100 grid md:mt-8 md:pt-4"></div>
-          <div className="grid "></div>
-
-          <button
-            className="col-span-2 mx-auto inline-flex items-center justify-center justify-self-center rounded-full bg-accent-1 px-5 py-2.5 text-center text-sm font-medium text-dark hover:bg-cyan focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 disabled:opacity-50 dark:hover:bg-[#050708]/30 dark:focus:ring-slate-500 md:mt-1 "
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            ) : (
-              <Icons.key className="mr-2 h-4 w-4 " />
-            )}
-            Sign In with Virtuous
-          </button>
-        </div>
+          Sign In with Virtuous
+        </button>
       </form>
     </div>
   )
