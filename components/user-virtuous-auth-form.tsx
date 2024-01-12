253909037aee92283034674def6097312682ccda
Joshua Bechard <josh.bechard@gmail.com>
2024-01-12 11:06:39 -0600

    linting

--- components/user-virtuous-auth-form.tsx
+++ components/user-virtuous-auth-form.tsx
@@ -94,7 +94,7 @@ export function UserVirtuousAuthForm({
               <input
                 id="twoFactor"
                 placeholder="123456"
-                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
                 type="text"
                 autoCapitalize="none"
                 autoCorrect="off"
@@ -112,7 +112,7 @@ export function UserVirtuousAuthForm({
               <input
                 id="email"
                 placeholder="name@example.com"
-                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-black placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-black placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
                 type="email"
                 autoCapitalize="none"
                 autoComplete="email"
@@ -133,7 +133,7 @@ export function UserVirtuousAuthForm({
               <input
                 id="password"
                 placeholder="password"
-                className="my-1 h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+                className="my-1 h-9 w-full rounded-full border border-slate-300 px-3 py-2 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
                 type="password"
                 autoCapitalize="none"
                 autoCorrect="off"
